0a6cdc7b9251cb06def3cb0b185e1f6839441596
Mark Ottaway <mark.ottaway@sohonet.com>
2019-01-18 14:38:09 +0000

    Ammended perl module

--- manifests/module/perl.pp
+++ manifests/module/perl.pp
@@ -3,17 +3,17 @@
 # Create the perl module configuration for FreeRADIUS
 #
 define freeradius::module::perl (
-  $ensure                              = 'file',
+  $ensure                              = file,
   String $moddir                       = "${fr_moduleconfigpath}/perl",
   Optional[String] $key                = undef,
   Optional[String] $perl_filename      = undef,
-  Optional[String] $source             = undef,
+  Optional[String] $path               = undef,
   Optional[String] $content            = undef,
 ) {
   $fr_moduleconfigpath = $::freeradius::params::fr_moduleconfigpath
   $fr_group            = $::freeradius::params::fr_group
   $fr_service          = $::freeradius::params::fr_service
-
+  $source              = "${path}/${perl_filename}"
   freeradius::module {'perl':
     ensure  => $ensure,
     content => template('freeradius/perl.erb'),
